af62a75d696a13deab4277c0c4bc009673d25f96
haruna <haruna_02@aliyun.com>
2023-12-19 23:44:20 +0800

    first commit

---
+++ src/main/java/com/haruna/GitTest.java
@@ -0,0 +1,12 @@
+package com.haruna;
+
+/**
+ * Description:
+ *
+ * @Author haruna
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
