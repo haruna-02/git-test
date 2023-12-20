76dacb87c0852aab1c91ba2ddba9e3a9c3f56782
haruna <haruna_02@aliyun.com>
2023-12-20 19:34:40 +0800

    second commit

--- src/main/java/com/haruna/GitTest.java
+++ src/main/java/com/haruna/GitTest.java
@@ -8,5 +8,7 @@ package com.haruna;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
+        System.out.println("Hello Git 2!");
+
     }
 }
