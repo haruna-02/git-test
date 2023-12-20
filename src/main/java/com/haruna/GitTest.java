eb0049593135d729d8773582963df348f62ed6fa
haruna <haruna_02@aliyun.com>
2023-12-20 19:56:01 +0800

    hot-fix commit1

--- src/main/java/com/haruna/GitTest.java
+++ src/main/java/com/haruna/GitTest.java
@@ -10,6 +10,7 @@ public class GitTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git 2!");
         System.out.println("Hello Git 3!");
+        System.out.println("Hello Git by hot-fix!");
 
     }
 }
